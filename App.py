f6b1359bc7b25c7f371513f25c565aa1c6971472
anonimo3967-alt <anonimo3967@gmail.com>
2026-05-03 13:08:05 -0500

    Update App.py

--- App.py
+++ App.py
@@ -31,7 +31,7 @@ st.text("Lo que va a hacer es que un modelo entrenado te va a decir que número 
 drawing_mode = "freedraw"
 stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
 color = st.color_picker("Selecciona un color a tu gusto", "#000000")
-stroke_color = 'color' # Set background color to white
+stroke_color = '#FFFFFF' # Set background color to white
 bg_color = '#000000'
 
 # Create a canvas component
